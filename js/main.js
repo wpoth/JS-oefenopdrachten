5963ee8742c548475c278d72eae0aeb49f212889
wpoth <302758116@student.rocmondriaan.nl>
2023-12-04 09:51:39 +0100

    .

--- js/main.js
+++ js/main.js
@@ -25,10 +25,10 @@
 //     }
 // }
 
-// // opdracht 3
+// opdracht 3
 
-const temperaturen = [
-  3.6, 3.9, 6.5, 9.9, 13.4, 16.2, 18.3, 17.9, 14.7, 10.9, 7.0, 4.2,
+// const temperaturen = [
+ [ 3.6, 3.9, 6.5, 9.9, 13.4, 16.2, 18.3, 17.9, 14.7, 10.9, 7.0, 4.2,
 ];
 
 // Vind de warmste temperatuur
@@ -43,34 +43,34 @@ console.log("De koudste temperatuur is: " + koudste);
 const bovenTien = temperaturen.filter((temp) => temp > 10);
 console.log("Temperaturen boven de 10 graden zijn: " + bovenTien.join(", "));
 
-// opdracht 4
+// // opdracht 4
 
-let weather = document.querySelector(".weather");
+// let weather = document.querySelector(".weather");
 
-const weatherList = [
-  { month: "januari", temperature: 3.6 },
+// const weatherList = [
+//   { month: "januari", temperature: 3.6 },
 
-  { month: "februari", temperature: 3.9 },
+//   { month: "februari", temperature: 3.9 },
 
-  { month: "maart", temperature: 6.5 },
+//   { month: "maart", temperature: 6.5 },
 
-  { month: "april", temperature: 9.9 },
+//   { month: "april", temperature: 9.9 },
 
-  { month: "mei", temperature: 13.4 },
+//   { month: "mei", temperature: 13.4 },
 
-  { month: "juni", temperature: 16.2 },
+//   { month: "juni", temperature: 16.2 },
 
-  { month: "juli", temperature: 18.3 },
+//   { month: "juli", temperature: 18.3 },
 
-  { month: "augustus", temperature: 17.9 },
+//   { month: "augustus", temperature: 17.9 },
 
-  { month: "oktober", temperature: 14.7 },
+//   { month: "oktober", temperature: 14.7 },
 
-  { month: "september", temperature: 10.9 },
+//   { month: "september", temperature: 10.9 },
 
-  { month: "november", temperature: 7 },
+//   { month: "november", temperature: 7 },
 
-  { month: "december", temperature: 4.2 },
-];
+//   { month: "december", temperature: 4.2 },
+// ];
 
-console.log(weatherList);
+// console.log(weatherList);
